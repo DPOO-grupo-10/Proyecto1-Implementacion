6680f306c61a2d5fffeaeb3fa4b6288c5eb0a84b
jvegav <j.vegav@uniandes.edu.co>
2023-03-26 14:26:52 -0500

    .

---
+++ src/Modelo/Cama.java
@@ -0,0 +1,15 @@
+package Modelo;
+
+public class Cama {
+	// existen tres tipos de cama: NIÑOS, SENCILLA Y DOBLE 
+	private String tamañoCama;
+	// CUANTAS PERSONAS CABEN SEGUN EL TIPO DE CAMA
+	private int ocupacion;
+	
+	
+	public Cama(String tamaño, int capacidad) {
+		this.tamañoCama = tamaño;
+		this.ocupacion = capacidad;
+	}
+
+}
