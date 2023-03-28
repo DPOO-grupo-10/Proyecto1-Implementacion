13c0c9c2c63f463da19740658b5929b869cd3181
Leonardo3310 <jl.rangel@uniandes.edu.co>
2023-03-28 12:00:44 -0500

    aa

--- src/Modelo/Consumo.java
+++ src/Modelo/Consumo.java
@@ -1,5 +1,12 @@
 package Modelo;
+import java.util.HashMap;
+import java.util.ArrayList;
 
 public class Consumo {
-    
+    // nota el String seria la habitacion a la que se le aplican los consumos
+    private HashMap<String,ArrayList> consumos = new HashMap<String,ArrayList>();
+
+    public HashMap<String,ArrayList> getConsumos(){
+        return consumos;
+    }
 }
