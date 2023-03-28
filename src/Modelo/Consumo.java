6b4a7678828f4c0fb96ddd3a3135feeb37d2aad1
Leonardo3310 <jl.rangel@uniandes.edu.co>
2023-03-28 12:13:34 -0500

    nais

--- src/Modelo/Consumo.java
+++ src/Modelo/Consumo.java
@@ -2,11 +2,32 @@ package Modelo;
 import java.util.HashMap;
 import java.util.ArrayList;
 
-public class Consumo {
-    // nota el String seria la habitacion a la que se le aplican los consumos
-    private HashMap<String,ArrayList> consumos = new HashMap<String,ArrayList>();
+import java.util.*;
 
-    public HashMap<String,ArrayList> getConsumos(){
+public class Consumo {
+    private Map<String, List<Servicio>> consumos;
+    
+    public Consumo() {
+        this.consumos = new HashMap<>();
+    }
+    
+    public void agregarConsumo(String nombreConsumo, Servicio servicio) {
+        if (!consumos.containsKey(nombreConsumo)) {
+            consumos.put(nombreConsumo, new ArrayList<>());
+        }
+        
+        consumos.get(nombreConsumo).add(servicio);
+    }
+    
+    public List<Servicio> obtenerServiciosPorConsumo(String nombreConsumo) {
+        if (!consumos.containsKey(nombreConsumo)) {
+            return new ArrayList<>();
+        }
+        
+        return consumos.get(nombreConsumo);
+    }
+    
+    public Map<String, List<Servicio>> obtenerConsumos() {
         return consumos;
     }
 }
